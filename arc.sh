1d3f57b014be2e4c776fd441cd4a9a70ce72da08
John Boyle <johnthescavenger+github@gmail.com>
2013-10-25 06:06:46 -0700

    Slight error message rewording

--- arc.sh
+++ arc.sh
@@ -5,8 +5,8 @@ then
     if which -s rlwrap
     then rlwrap -cr -q "\"" -C arc racket -if arc3.1/as.scm
     else
-        if (($# > 0))
-            then echo "Installing rlwrap will make your life easier."
+        if (($# == 0))
+            then echo "Installing rlwrap is optional but will make your life easier."
                  echo "(Turn this warning off by supplying any arguments to this script.)"
         fi
         racket -if arc3.1/as.scm
