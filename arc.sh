72f49483cdcbb77e5c17b814b9d028f0ea953a88
John Boyle <johnthescavenger+github@gmail.com>
2013-10-24 20:00:10 -0700

    a tool to run Arc

---
+++ arc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if which -s racket
+then
+    if which -s rlwrap
+    then rlwrap -cr -q "\"" -C arc racket -if arc3.1/as.scm
+    else
+        if (($# > 0))
+            then echo "Installing rlwrap will make your life easier."
+                 echo "(Turn this warning off by supplying any arguments to this script.)"
+        fi
+        racket -if arc3.1/as.scm
+    fi
+else echo "Install Racket, ensuring that the command-line executable \"racket\""
+     echo "is accessible by that name."
+fi
+
+
